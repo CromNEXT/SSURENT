72bf676ba20954d90e04342dd71d6dbfb51ea9a0
Crom <doyoung.yang2005@gmail.com>
2025-08-10 22:59:33 +0900

    Feat : 메인페이지 구성
    - 메인 페이지 텍스트 추가
    - 드래그 방지 추가
    - 대여/반납 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-    return <h1>홈</h1>;
+import React from 'react';
+import '../components/Home.css'
+import {Link} from "react-router-dom";
+
+function Home() {
+    return (
+        <main className="main-content">
+            <h1>학교에서 급하게 필요한</h1>
+            <h1>물품이 있다면?</h1>
+            <h2>지금 바로 SSURENT에서!</h2>
+            <div className="button-container">
+                <Link to="/rent">
+                    <button type="button" className="btn btn-primary">물품대여 바로가기</button>
+                </Link>
+                <Link to="/return">
+                    <button type="button" className="btn btn-primary">물품반납 바로가기</button>
+                </Link>
+            </div>
+        </main>
+    );
 }
+
+export default Home;
