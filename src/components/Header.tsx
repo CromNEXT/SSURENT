67fb31bdf3286a478beffacbf2a2a0beeaa5ad57
Crom <doyoung.yang2005@gmail.com>
2025-08-10 22:09:45 +0900

    Feat : 상단 네비게이션 바 분리 - 상단 네비게이션 바 Header.tsx로 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/images/ssulogo.jpg"; // 이미지 경로는 실제 프로젝트에 맞게 확인해주세요.
+import "../App.css";
+
+export default function Header() {
+    const linkClass = ({ isActive }: { isActive: boolean }) =>
+        "nav-link px-3 link-dark fw-bold" + (isActive ? " active" : "");
+
+    return (
+        // 1. 전체를 <div className="container">로 감싸줍니다.
+        <div className="container">
+            <header className="d-flex flex-wrap align-items-center py-3 mb-4 border-bottom">
+
+                <NavLink to="/"
+                         className="d-flex align-items-center text-dark text-decoration-none">
+                    <img src={logo} alt="로고 이미지" style={{width: '90px'}} className="img-fluid me-2"/>
+                    <span className="fs-4">SSURENT</span>
+                </NavLink>
+
+                <ul className="nav me-auto ms-5">
+                    <li><NavLink to="/rent" className={linkClass}>대여하기</NavLink></li>
+                    <li><NavLink to="/return" className={linkClass}>반납하기</NavLink></li>
+                    <li><NavLink to="/info" className={linkClass}>정보</NavLink></li>
+                    <li><NavLink to="/mypage" className={linkClass}>마이페이지</NavLink></li>
+                </ul>
+
+                <div className="text-end me-5">
+                    <button type="button" className="btn btn-outline-primary ">로그인</button>
+                </div>
+            </header>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        {/* 하위 경로 배포 시: <BrowserRouter basename="/ssurent"> */}
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
