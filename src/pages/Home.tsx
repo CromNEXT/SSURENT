a179a0a597ddbe54a4ccfdc2c9977637af9c486e
Crom <doyoung.yang2005@gmail.com>
2025-08-17 00:31:54 +0900

    Feat : 메인페이지 아래 안내 문구 추가
    - 대여사업 설명 문구 및 카드 섹션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../components/Home.css'
 import {Link} from "react-router-dom";
+import {PersonFill} from "react-bootstrap-icons";
+import {Building} from "react-bootstrap-icons";
+import {Alarm} from "react-bootstrap-icons";
 
 function Home() {
     return (
@@ -15,6 +18,38 @@ function Home() {
                 <Link to="/return">
                     <button type="button" className="btn btn-primary">물품반납 바로가기</button>
                 </Link>
+            </div>
+            <div className="middle-describes">
+                <h2 >대여사업이 뭔가요</h2>
+                <br/>
+                <h4>
+                    컴퓨터학부 학우분들의 보다 나은 생활을 위해<br/>
+                    컴퓨터학부의 학생회비 납부자를 대상으로<br/>
+                    물품 대여 사업을 진행하고 있습니다
+                </h4>
+            </div>
+            <div className="info-cards-container">
+                <div className="info-card">
+                    <PersonFill size={80} color="#000000" />
+                    <br/>
+                    <br/>
+                    <h3>이용 대상</h3>
+                    <p>컴퓨터학부 학생회비 납부자</p>
+                </div>
+                <div className="info-card">
+                    <Building size={80} color="#000000" />
+                    <br/>
+                    <br/>
+                    <h3>대여 장소</h3>
+                    <p>정보과학관 B1층 컴퓨터학부 학생회실</p>
+                </div>
+                <div className="info-card">
+                    <Alarm size={80} color="#000000" />
+                    <br/>
+                    <br/>
+                    <h3>이용 시간</h3>
+                    <p>평일 10:00 ~ 16:00</p>
+                </div>
             </div>
         </main>
     );
